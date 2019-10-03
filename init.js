bb01af1883f23e21605a6517b6edd4b122d02610
Nick Anderson <nick.anderson@omadahealth.com>
2019-10-03 12:40:01 -0700

    Add vault step to starting the app. Add deets to readme.

--- init.js
+++ init.js
@@ -2,4 +2,26 @@
 require('safestart')(__dirname);
 require('babel-core/register');
 require('babel-polyfill');
-require('dotenv').config({ silent: true });
+
+const vault = require('node-vault');
+async function buildSecretsBuffer() {
+  const vaultClient = vault({
+    endpoint: process.env.VAULT_ADDR,
+    token: process.env.VAULT_TOKEN,
+  });
+
+  const secrets = await vaultClient.list(`apps/outline/staging`, {
+    format: 'json',
+  });
+  const { keys } = secrets.data;
+  let output = '';
+  for (let key of keys) {
+    const read = await vaultClient.read(`apps/outline/staging/${key}`);
+    const { value } = read.data;
+    output += `${key.toUpperCase()}=${value}\n`;
+  }
+
+  return Buffer.from(output);
+}
+
+require('dotenv').config({ path: buildSecretsBuffer(), silent: true });
