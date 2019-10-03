d050db7714172fff1cb1a616ea736afefb2c59ac
Nick Anderson <nick.anderson@omadahealth.com>
2019-10-03 13:13:42 -0700

    Rename ECR repo

--- jenkins/image_name.sh
+++ jenkins/image_name.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ECR_REPO="661956145551.dkr.ecr.us-west-2.amazonaws.com/omada-registry/b2b/outline"
+ECR_REPO="661956145551.dkr.ecr.us-west-2.amazonaws.com/omada-registry/engineering/outline"
 if [ -z "${BRANCH_NAME}" ]; then
   BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 fi
