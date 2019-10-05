886f88d002aee2fb40e100c2f165e5358668f18e
Nick Anderson <nick.anderson@omadahealth.com>
2019-10-04 18:17:02 -0700

    Fixes ECONNRESET problem when connecting to redis.
    Dont verify certs when connecting to redis. Not ideal but :shrug:

--- server/redis.js
+++ server/redis.js
@@ -1,6 +1,8 @@
 // @flow
 import redis from 'redis';
 
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient(process.env.REDIS_URL, {
+  tls: { checkServerIdentity: () => undefined },
+});
 
 export { client };
